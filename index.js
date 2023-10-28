39ca1f6013e427b8b2b5f6823acd873a778cd910
5kydriven <tagpuno619@gmail.com>
2023-10-28 09:47:06 +0800

    created a function playRound

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ function getComputerChoice () {
     } else if (choice === 1) {
         return "Paper";
     } else {
-        return "Scsissor";
+        return "Scissor";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose!";
+    } else if (playerSelection === "Scissor" && computerSelection === "Rock") {
+        return "You Lose!";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissor") {
+        return "You Lose!";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Win!";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
+        return "You Win!";
+    } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
+        return "You Win!";
+    } else {
+        return "Unkown Keyword"; 
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
