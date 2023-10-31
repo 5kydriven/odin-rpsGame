8a310fd444d686ff7b0796a38e7d0d3a2af37544
5kydriven <tagpuno619@gmail.com>
2023-10-31 10:46:27 +0800

    added function scoring. missing deciding for winner

--- index.js
+++ index.js
@@ -1,57 +1,52 @@
-function getComputerChoice() {
+let comp = () => {
   const choice = Math.floor(Math.random() * 3);
   if (choice === 1) {
-    return "rock";
+    computerImg.src = "./images/rock.png";
+    return 'rock';
   } else if (choice === 2) {
-    return "paper";
+    computerImg.src = "./images/paper.png";
+    return 'paper';
   } else {
-    return "scissor";
+    computerImg.src = "./images/scissor.png";
+    return 'scissor';
   }
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "paper") {
-    ComputerScore++;
-  } else if (playerSelection === "scissor" && computerSelection === "rock") {
-    ComputerScore++;
-  } else if (playerSelection === "paper" && computerSelection === "scissor") {
-    ComputerScore++;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerScore++;
-  } else if (playerSelection === "rock" && computerSelection === "scissor") {
-    playerScore++;
-  } else if (playerSelection === "scissor" && computerSelection === "paper") {
-    playerScore++;
-  } else if (playerSelection === "paper" && computerSelection === "paper") {
-    draw++;
-  } else if (playerSelection === "rock" && computerSelection === "rock") {
-    draw++;
-  } else if (playerSelection === "scissor" && computerSelection === "scissor") {
-    draw++;
+function playerSelection(bet) {
+  if (bet.innerText.toLowerCase() === 'rock') {
+    playerImg.src = "./images/rock.png";
+  } else if (bet.innerText.toLowerCase() === 'paper') {
+    playerImg.src = "./images/paper.png";
   } else {
-    return "Unkown Keyword";
+    playerImg.src = "./images/scissor.png";
   }
+  pChoice =  bet.innerText.toLowerCase();
 }
 
-function game(e) {
-    if (e === "y") {
-        for (let i = 1; i <= round; i++) {
-            const playerSelection = prompt("Enter your bet: ");
-            const computerSelection = getComputerChoice();
-            playRound(playerSelection, computerSelection);
-        }
-        console.log("Player: " + playerScore + " Computer: " + ComputerScore + " Draw: " + draw);
-        playAgain = prompt("Wanna play again? (y/n)");
-    } else {
-        console.log("Ok!");
-    }
+function game(player, computer) {
+  if (player === 'rock' && computer === 'paper' || player === 'scissor' && computer === 'rock' || player === 'paper' && computer === 'scissor') cScore.innerText++;
+  if (player === 'rock' && computer === 'scissor' || player === 'scissor' && computer === 'paper' || player === 'paper' && computer === 'rock') pScore.innerText++;
 }
 
-let round = 5;
-let playerScore = 0;
-let ComputerScore = 0;
-let draw = 0;
-let playAgain = prompt("Wanna play? (y/n)");
-game(playAgain);
+
+
+let pChoice = '';
+const btn = document.querySelectorAll('button');
+const playerImg = document.querySelector(".playerBet");
+const computerImg = document.querySelector(".computerBet");
+let pScore = document.querySelector(".player");
+let cScore = document.querySelector(".computer");
+
+
+btn.forEach(btns => {
+  if (pScore.innerText !== 5 || cScore.innerText !== 5) {
+    btns.addEventListener('click', () => {
+      game(pChoice, comp());
+      console.log(pScore.innerText);
+    })
+  } else {
+    pScore.innerText === 5 ? alert("You Win!") : alert("Computer Win!");
+  }
+});
 
 
